4d21cca750a9891d002e138e2902d65905a9ebca
Sakthieswaran <sakthieswaran.k@simplify3x.com>
2023-06-25 23:05:51 +0530

    auth screen design

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,4 +1,6 @@
 import 'package:ecommproject/constants/global_variables.dart';
+import 'package:ecommproject/features/auth/widgets/custom_button.dart';
+import 'package:ecommproject/features/auth/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 enum Auth { signin, signup }
@@ -13,6 +15,13 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   Auth _auth = Auth.signup;
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final _signInFormKey = GlobalKey<FormState>();
+  final _signUpFormKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +30,7 @@ class _AuthScreenState extends State<AuthScreen> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Text(
                 "Welcome",
@@ -28,6 +38,9 @@ class _AuthScreenState extends State<AuthScreen> {
               ),
               ListTile(
                 title: const Text('Create Account'),
+                tileColor: _auth == Auth.signup
+                    ? GlobalVariables.backgroundColor
+                    : GlobalVariables.greyBackgroundCOlor,
                 leading: Radio(
                     activeColor: GlobalVariables.secondaryColor,
                     value: Auth.signup,
@@ -38,8 +51,45 @@ class _AuthScreenState extends State<AuthScreen> {
                       });
                     }),
               ),
+              if (_auth == Auth.signup)
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: GlobalVariables.backgroundColor,
+                  child: Form(
+                    key: _signInFormKey,
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          textEditingController: _nameController,
+                          hintText: 'Name',
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CustomTextField(
+                          textEditingController: _emailController,
+                          hintText: 'Email',
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CustomTextField(
+                          textEditingController: _passwordController,
+                          hintText: 'Password',
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CustomButton(onTap: () {}, text: 'Sign Up')
+                      ],
+                    ),
+                  ),
+                ),
               ListTile(
                 title: const Text('Sign In'),
+                tileColor: _auth == Auth.signin
+                    ? GlobalVariables.backgroundColor
+                    : GlobalVariables.greyBackgroundCOlor,
                 leading: Radio(
                   activeColor: GlobalVariables.secondaryColor,
                   value: Auth.signin,
@@ -50,7 +100,34 @@ class _AuthScreenState extends State<AuthScreen> {
                     });
                   },
                 ),
-              )
+              ),
+              if (_auth == Auth.signin)
+                Container(
+                  color: GlobalVariables.backgroundColor,
+                  padding: const EdgeInsets.all(8),
+                  child: Form(
+                    key: _signUpFormKey,
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          textEditingController: _emailController,
+                          hintText: 'Email',
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CustomTextField(
+                          textEditingController: _passwordController,
+                          hintText: 'Password',
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CustomButton(onTap: () {}, text: 'Sign In')
+                      ],
+                    ),
+                  ),
+                )
             ],
           ),
         ),
---
+++ lib/features/auth/widgets/custom_text_field.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField(
+      {required this.textEditingController, required this.hintText, super.key});
+
+  final TextEditingController textEditingController;
+  final String hintText;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: textEditingController,
+      decoration: InputDecoration(
+          hintText: hintText,
+          border: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black38)),
+          enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black38))),
+      validator: (val) {
+        return "";
+      },
+    );
+  }
+}
